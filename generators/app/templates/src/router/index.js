0a519a51be9e681a0245f3b7c6321622307b7270
QiuShichang <shichang.qiu@hotmail.com>
2022-06-16 17:57:36 +0800

    feat(style): st

--- generators/app/templates/src/router/index.js
+++ generators/app/templates/src/router/index.js
@@ -12,6 +12,11 @@ const router = createRouter({
       component: Home,
     },
     {
+      path: "/home",
+      name: "home",
+      component: Home,
+    },
+    {
       path: "/about",
       name: "about",
       component: About,
