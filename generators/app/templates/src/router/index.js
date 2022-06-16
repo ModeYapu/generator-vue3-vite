968d36aea85e303caef3e2c03ddf34b3b68ea574
QiuShichang <shichang.qiu@hotmail.com>
2022-06-16 18:40:35 +0800

    feat(style): style

--- generators/app/templates/src/router/index.js
+++ generators/app/templates/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
