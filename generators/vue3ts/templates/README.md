e6e7daf2d0bf4a4eccff94d8e6792cd1f7ce1e14
QiuShichang <shichang.qiu@hotmail.com>
2022-06-21 23:40:05 +0800

    feat(vue3ts): ts项目优化

--- generators/vue3ts/templates/README.md
+++ generators/vue3ts/templates/README.md
@@ -1,2 +1,3 @@
 # Vue 3 + Typescript + Vite
 
+## 项目路由配置采用 vue-router动态路由+ defineAsyncComponent 动态组件 免除路由的繁琐配置
--- generators/vue3ts/templates/src/views/home.vue
+++ generators/vue3ts/templates/src/views/home.vue
@@ -6,9 +6,8 @@
 <script setup lang="ts">
 import headerMenu from "@/components/menu.vue";
 import ConsultingService from "@/components/consulting-service.vue";
-import { onMounted, onUnmounted, reactive, ref } from "vue";
-import router from "../router";
 
+// 动态图片加载
 const getImage = (name: string): string => {
   return new URL(`../assets/img/${name}`, import.meta.url).href;
 };
